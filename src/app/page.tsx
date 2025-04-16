c32c53140c084885eb74d5ec21450b7c26977214
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-04-16 00:53:34 -0400

    add current location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "@/app/page.module.css";
 import Button from "@/components/button/page";
+import Image from "next/image";
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
@@ -14,6 +15,12 @@ export default function LandingPage() {
       <h2 className={`${poppins.className} ${styles.subtitle}`}>I am a Data Engineer in training</h2>
       <p className={`${poppins.className} ${styles.description}`}>I'm a data engineer in training, specializing in building data pipelines and cloud computing to help business grow with data driven insights. Currently, <span className={styles.highlight}>I’m looking for a full time role in Toronto, starting in January 2026</span></p>
       <Button text="Check out my projects!" href="/projects"/>
+      <div className={`${poppins.className} ${styles.locationContainer}`}>
+        <div className={styles.locationIconContainer}>
+          <Image src="/icons/pin.svg" alt="Location" layout="fill" objectFit="contain"/>
+        </div>
+        <p>London, ON</p>
+      </div>
     </div>
   );
 }
