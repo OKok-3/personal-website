598dd3c2f7aa9ad835cc0193b6f2d02e1fab2ef0
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-04-15 22:21:33 -0400

    implement nav bar

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,5 +1,48 @@
-import styles from "@/components/header/page.modeul.css";
+"use client";
+
+import styles from "@/components/header/page.module.css";
+import { usePathname } from "next/navigation";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+
+const poppins = Poppins({
+  weight: ["500", "600"],
+  subsets: ["latin"],
+});
 
 export default function Header() {
-  return <></>;
+  const currentPath = usePathname().replace(/^\//, "");
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image className={styles.logoImage} src="/logo.png" alt="logo" layout="fill" objectFit="contain" />
+          </Link>
+        </div>
+        <nav className={styles.nav}>
+          <ul className={`${styles.navList} ${poppins.className}`}>
+            <li className={styles.navItem}>
+              <Link href="/" className={`${styles.link} ${currentPath === "" ? styles.active : ""}`}>
+                Home
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/projects" className={`${styles.link} ${currentPath === "projects" ? styles.active : ""}`}>
+                Projects
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="/about" className={`${styles.link} ${currentPath === "about" ? styles.active : ""}`}>
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
