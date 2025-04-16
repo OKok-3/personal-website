241de538c29bd8cb580d5a79ffd44b6e1e3539d6
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-04-16 00:46:57 -0400

    implement social media icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header/page";
 import Footer from "@/components/footer/page";
+import SocialsContainer from "@/components/socialsContainer/page";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Header />
         <main>{children}</main>
+        <SocialsContainer />
         <Footer />
       </body>
     </html>
---
+++ src/components/socialsContainer/page.tsx
@@ -0,0 +1,26 @@
+import styles from "@/components/socialsContainer/page.module.css";
+import Image from "next/image";
+
+export default function SocialsContainer() {
+  return (
+    <div className={styles.socialsContainer}>
+        <div className={styles.socialIconContainer}>
+          <a href="https://www.linkedin.com/in/tong-g" target="_blank" rel="noopener noreferrer">
+            <Image className={styles.socialIcon} src="/icons/linkedin.svg" alt="LinkedIn" layout="fill" objectFit="contain" />
+          </a>
+        </div>
+        <div className={styles.socialIconContainer}>
+          <a href="https://github.com/OKok-3" target="_blank" rel="noopener noreferrer">
+            <Image className={styles.socialIcon} src="/icons/github.svg" alt="GitHub" layout="fill" objectFit="contain" />
+          </a>
+        </div>
+        <div className={styles.socialIconContainer}>
+          <a href="mailto:dguan.msc2025@ivey.ca" target="_blank" rel="noopener noreferrer">
+            <Image className={styles.socialIcon} src="/icons/email.svg" alt="Email" layout="fill" objectFit="contain" />
+          </a>
+        </div>
+        <div className={styles.verticalLine}></div>
+      </div>
+  );
+}
+
