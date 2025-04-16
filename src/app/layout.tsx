3ed6cb1e0754fa63178d45570374d83997c1859e
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-04-16 19:22:16 -0400

    add viewport specification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,13 @@ export const metadata = {
   description: 'Daniel\'s personal website'
 }
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
