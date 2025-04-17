2e9e4f1ef78b522aa76384eebafaf8e941d2c3e9
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-04-17 02:43:29 -0400

    extrapolate text values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,26 @@ const poppins = Poppins({
 });
 
 export default function LandingPage() {
+  const title = "Hi, my name is Daniel"
+  const subtitle = "I am a Data Engineer in training"
+  const description = "I'm a data engineer in training, specializing in building data pipelines and cloud computing to help business grow with data driven insights."
+  const highlight = "Currently, I'm looking for a full time role in Toronto, or a remote role in Canada, starting in January 2026"
+  const buttonText = "Check out my projects!"
+  const location = "London, ON"
+
+
   return (
     <div className={styles.container}>
-      <h1 className={`${poppins.className} ${styles.title}`}>Hi, my name is Daniel</h1>
-      <h2 className={`${poppins.className} ${styles.subtitle}`}>I am a Data Engineer in training</h2>
-      <p className={`${poppins.className} ${styles.description}`}>I'm a data engineer in training, specializing in building data pipelines and cloud computing to help business grow with data driven insights.</p>
-      <p className={`${poppins.className} ${styles.highlight}`}>Currently, I'm looking for a full time role in Toronto, or a remote role in Canada, starting in January 2026</p>
-      <Button text="Check out my projects!" href="/projects"/>
+      <h1 className={`${poppins.className} ${styles.title}`}>{title}</h1>
+      <h2 className={`${poppins.className} ${styles.subtitle}`}>{subtitle}</h2>
+      <p className={`${poppins.className} ${styles.description}`}>{description}</p>
+      <p className={`${poppins.className} ${styles.highlight}`}>{highlight}</p>
+      <Button text={buttonText} href="/projects"/>
       <div className={`${poppins.className} ${styles.locationContainer}`}>
         <div className={styles.locationIconContainer}>
           <Image src="/icons/pin.svg" alt="Location" fill={true} style={{ objectFit: "contain" }} />
         </div>
-        <p>London, ON</p>
+        <p>{location}</p>
       </div>
     </div>
   );
