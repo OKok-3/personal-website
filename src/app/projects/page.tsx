518b1fac7562009b8d1ca9f65336504ae3182b9a
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-05-02 14:33:58 -0400

    style: add wrapper to show left border and set tech stack item spacing

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -42,9 +42,11 @@ export default function Projects() {
                 </motion.div>
                 <motion.div className={styles.techStackContainer} variants={staggerChildren({ delayChildren: 1.5, staggerChildren: 0.3 })}>
                     <motion.h3 className={`${poppins.className} ${styles.techStackTitle}`} variants={fadeIn({ duration: 0.5 })}>My Professional Tech Stack</motion.h3>
-                    <TechStackItem title="proficient in:" skills={["python", "postgres", "r"]} />
-                    <TechStackItem title="practical experience in:" skills={["pandas", "numpy", "scikit-learn", "matplotlib", "seaborn", "plotly", "spark", "react", "nextjs","docker",  "github-actions", "pve", "debian", "ubuntu", "java", "confluence", "jira"]} />
-                    <TechStackItem title="learning:" skills={["aws", "snowflake", "dbt"]} />
+                    <motion.div className={styles.techStackItemsContainer} variants={fadeIn({ duration: 0.5, when: "beforeChildren", staggerChildren: 0.3 })}>
+                        <TechStackItem title="proficient in:" skills={["python", "postgres", "r"]} />
+                        <TechStackItem title="practical experience in:" skills={["pandas", "numpy", "scikit-learn", "matplotlib", "seaborn", "plotly", "spark", "react", "nextjs","docker",  "github-actions", "pve", "debian", "ubuntu", "java", "confluence", "jira"]} />
+                        <TechStackItem title="learning:" skills={["aws", "snowflake", "dbt"]} />
+                    </motion.div>
                 </motion.div>
             </div>
         </>
