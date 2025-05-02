b8e451cfdd3c8eefd354593f062faa856f422abc
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-05-01 23:21:36 -0400

    feature: implement featured project card (contents for demo only)

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,6 +5,7 @@ import { splitText } from "@/components/utils/helpers";
 import { motion } from "motion/react";
 import { Poppins } from "next/font/google";
 import { staggerChildren } from "@/utils/animationVariants";
+import FeaturedProjectCard from "@/components/ProjectCard/Featured/page";
 
 const poppins = Poppins({
     weight: ["400", "500", "600", "700"],
@@ -15,10 +16,25 @@ export default function Projects() {
     const title = splitText("Featured Projects");
 
     return (
-        <motion.div className={styles.projectsContainer}>
+        <>
             <motion.h1 className={`${poppins.className} ${styles.title}`} variants={staggerChildren({ staggerChildren: 0.05 })}>
                 {title}
             </motion.h1>
-        </motion.div>
+
+            <motion.div className={styles.featuredProjectsContainer}>
+                <motion.div className={styles.projectsContainer}>
+                    <FeaturedProjectCard
+                        title="Monte Carlo Simulation"
+                        description="Portfolio optimization using Monte Carlo Simulation on a portfolio of 30 stocks, based on metrics like Sharpe Ratio, Maximum Drawdown, and more"
+                        image="/ProjectImages/monte_carlo.png"
+                        link={{ link: "https://www.github.com", logo: "/icons/github.svg" }}
+                        tags={["Python", "Quant Finance"]}
+                    />
+                </motion.div>
+                <motion.div className={styles.techStackContainer}>
+
+                </motion.div>
+            </motion.div>
+        </>
     );
 }
