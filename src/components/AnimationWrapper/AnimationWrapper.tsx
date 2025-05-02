e13c152b45a5061b7175857bd5d53c0dca60770c
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-05-01 21:14:39 -0400

    bug: fix styling issues caused by AnimationWrapper

--- src/components/AnimationWrapper/AnimationWrapper.tsx
+++ src/components/AnimationWrapper/AnimationWrapper.tsx
@@ -3,7 +3,7 @@
 import { motion } from "motion/react";
 import { useState } from 'react';
 import { AnimationContext } from "@/contexts/animation/AnimationContext";
-
+import styles from "./styles.module.css";
 /**
  * Wrapper component for the animation. Used for exit animations. Header links will first change the state to exit, wait for the animation to complete, 
  * then push the route to the new page.
@@ -27,7 +27,7 @@ export default function AnimationWrapper({ children }: { children: React.ReactNo
 
   return (
     <AnimationContext.Provider value={{ isExiting, setIsExiting, exited, setExited }}>
-      <motion.div initial="hidden" animate={isExiting ? "exit" : "visible"} exit="exit" onAnimationComplete={handleAnimationComplete}>
+      <motion.div className={styles.container} initial="hidden" animate={isExiting ? "exit" : "visible"} exit="exit" onAnimationComplete={handleAnimationComplete}>
         {children}
       </motion.div>
     </AnimationContext.Provider>
