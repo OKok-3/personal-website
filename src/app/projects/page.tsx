8c8bf285b7cfdbf90bae32c9242d909962cefcec
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-05-01 21:25:40 -0400

    feature: implemented projects page title

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,24 @@
+'use client';
+
 import styles from "@/app/projects/page.module.css";
+import { splitText } from "@/components/utils/helpers";
+import { motion } from "motion/react";
+import { Poppins } from "next/font/google";
+import { staggerChildren } from "@/utils/animationVariants";
+
+const poppins = Poppins({
+    weight: ["400", "500", "600", "700"],
+    subsets: ["latin"],
+});
 
 export default function Projects() {
-    return <></>;
+    const title = splitText("Featured Projects");
+
+    return (
+        <motion.div className={styles.projectsContainer}>
+            <motion.h1 className={`${poppins.className} ${styles.title}`} variants={staggerChildren({ staggerChildren: 0.05 })}>
+                {title}
+            </motion.h1>
+        </motion.div>
+    );
 }
