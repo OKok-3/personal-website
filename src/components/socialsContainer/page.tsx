f9a5b0c3adef3be3053aa5b754ac06969e22687b
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-05-04 19:40:38 -0400

    style: update fade-in animation duration for social icons

--- src/components/socialsContainer/page.tsx
+++ src/components/socialsContainer/page.tsx
@@ -6,6 +6,7 @@ import { motion } from "motion/react";
 import Link from "next/link";
 import { fadeIn, staggerChildren } from "@/utils/animationVariants";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function SocialsContainer() {
   const currentPath = usePathname()?.replace(/^\//, "") || "";
@@ -21,25 +22,22 @@ export default function SocialsContainer() {
   };
 
   return (
-    <motion.div className={styles.socialsContainer} 
-      initial="hidden"
-      exit="exit"
-      animate="visible"
-      variants={staggerChildren({ staggerChildren: -0.3, delayChildren: delay + 2.5 })}
+    <motion.div className={styles.socialsContainer}
+      variants={staggerChildren({ staggerChildren: -0.3, delayChildren: delay })}
     >
-      <motion.div className={styles.socialIconContainer} variants={fadeIn({ duration: 1 })}>
+      <motion.div className={styles.socialIconContainer} variants={fadeIn({ duration: 0.8 })}>
         <Link href="https://www.linkedin.com/in/tong-g" target="_blank" rel="noopener noreferrer">
           <Image className={styles.socialIcon} src="/icons/linkedin.svg" alt="LinkedIn" fill={true} style={{ objectFit: "contain" }} />
         </Link>
       </motion.div>
       
-      <motion.div className={styles.socialIconContainer} variants={fadeIn({ duration: 1 })}>
+      <motion.div className={styles.socialIconContainer} variants={fadeIn({ duration: 0.8 })}>
         <Link href="https://github.com/OKok-3" target="_blank" rel="noopener noreferrer">
           <Image className={styles.socialIcon} src="/icons/github.svg" alt="GitHub" fill={true} style={{ objectFit: "contain" }} />
         </Link>
       </motion.div>
       
-      <motion.div className={styles.socialIconContainer} variants={fadeIn({ duration: 1 })}>
+      <motion.div className={styles.socialIconContainer} variants={fadeIn({ duration: 0.8 })}>
         <Link href="mailto:dguan.msc2025@ivey.ca" target="_blank" rel="noopener noreferrer">
           <Image className={styles.socialIcon} src="/icons/email.svg" alt="Email" fill={true} style={{ objectFit: "contain" }} />
         </Link>
