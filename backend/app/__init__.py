a09d845b24256ab2bdd58a5a971a999943108654
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-05-06 18:20:11 -0400

    feat: implement user model

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask  # noqa: D104
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.orm import DeclarativeBase
 from app.utils import load_config
 
+
 # Instantiate the database extension
-db = SQLAlchemy()
+class Base(DeclarativeBase):  # noqa: D101
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
 
 
 def create_app():
@@ -16,5 +21,9 @@ def create_app():
 
     # Initialize the database extension
     db.init_app(app)
+
+    # Create the database tables
+    with app.app_context():
+        db.create_all()
 
     return app
---
+++ backend/app/models.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+from app import db
+from argon2 import PasswordHasher, Type
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(db.Model):
+    """User model.
+
+    Attributes:
+        id (int): The user's ID.
+        username (str): The user's username.
+        email (str): The user's email.
+        password (str): The user's password.
+        created_at (datetime): The user's creation date.
+        updated_at (datetime): The user's last update date.
+    """
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(nullable=True, unique=True)
+    __pw_hash: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now())
+    updated_at: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now())
+    last_login: Mapped[datetime] = mapped_column(nullable=True)
+
+    @property
+    def password(self) -> None:  # noqa: D102
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, raw_password: str) -> None:
+        self.__pw_hash = PasswordHasher().hash(
+            password=raw_password,
+            time_cost=16,
+            memory_cost=1024 * 16,
+            parallelism=1,
+            salt_len=32,
+            hash_len=256,
+            type=Type.ID,
+        )
+
+    def check_password(self, raw_password: str) -> bool:
+        """Check the user's password.
+
+        Args:
+            raw_password (str): The raw password to check.
+        """
+        return PasswordHasher().verify(
+            password=raw_password,
+            hash=self.__pw_hash,
+        )
+
+    def to_dict(self) -> dict:  # noqa: D102
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+            "last_login": self.last_login,
+        }
