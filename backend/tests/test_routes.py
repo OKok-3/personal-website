2c79e2138d887daa2d848ae6833119a444214b8b
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-05-10 04:40:25 -0400

    test: refactor create user function to make retrieiving created user information easier

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -3,6 +3,10 @@ from flask.testing import FlaskClient
 from flask import Flask
 import base64
 from flask_sqlalchemy import SQLAlchemy
+from app.models import Users
+
+USERNAME = "testuser"
+PASSWORD = "Testuser123!"
 
 
 @pytest.fixture(scope="module")
@@ -18,9 +22,20 @@ def clear_database(db: SQLAlchemy):
     db.create_all()
 
 
-def create_user(client: FlaskClient, username: str = "testuser", password: str = "Testuser123!"):
-    """Create a user."""
-    return client.post("/api/auth/register", json={"username": username, "password": password})
+def create_user(client: FlaskClient, username: str = USERNAME, password: str = PASSWORD) -> Users:
+    """Create a user.
+
+    Args:
+        client: The test client.
+        username: The username of the user.
+        password: The password of the user.
+
+    Returns:
+        Users: The created user.
+    """
+    response = client.post("/api/auth/register", json={"username": username, "password": password})
+    user = Users.query.filter_by(username=username).one_or_none()
+    return user, response
 
 
 class TestAuthRoutes:
@@ -28,49 +43,48 @@ class TestAuthRoutes:
 
     def test_register_user(self, client: FlaskClient):
         """Test the register user route."""
-        response = create_user(client)
+        _, response = create_user(client)
         assert response.status_code == 200
         assert response.json["message"] == "User registered successfully"
 
     def test_register_user_existing(self, client: FlaskClient):
         """Test the register user route with an existing user."""
         create_user(client)
-        response = create_user(client)
+        _, response = create_user(client)
 
         assert response.status_code == 401
         assert response.json["error"] == "User already exists"
 
     def test_register_user_missing_username(self, client: FlaskClient):
         """Test the register user route with a missing username."""
-        response = create_user(client, username=None)
+        _, response = create_user(client, username=None)
         assert response.status_code == 400
         assert response.json["error"] == "Missing username or password"
 
     def test_register_user_missing_password(self, client: FlaskClient):
         """Test the register user route with a missing password."""
-        response = create_user(client, password=None)
+        _, response = create_user(client, password=None)
         assert response.status_code == 400
         assert response.json["error"] == "Missing username or password"
 
     def test_login_user_empty_credentials(self, client: FlaskClient):
         """Test the login user route with empty credentials."""
-        response = create_user(client, username="", password="")
+        _, response = create_user(client, username="", password="")
         assert response.status_code == 400
         assert response.json["error"] == "Missing username or password"
 
     def test_register_user_invalid_password(self, client: FlaskClient):
         """Test the register user route with an invalid password."""
-        response = create_user(client, password="Invalidpassword")
+        _, response = create_user(client, password="Invalidpassword")
         assert response.status_code == 400
         assert response.json["error"] is not None
 
     def test_login_user(self, client: FlaskClient):
         """Test the login user route."""
-        create_user(client)
-        encoded_credentials = base64.b64encode(b"testuser:Testuser123!").decode("utf-8")
+        _, response = create_user(client)
+        credentials = f"{USERNAME}:{PASSWORD}"
+        encoded_credentials = base64.b64encode(credentials.encode("utf-8")).decode("utf-8")
         response = client.post("/api/auth/login", headers={"Authorization": f"Basic {encoded_credentials}"})
-
-        print(response.data.decode("utf-8"))
 
         assert response.json["message"] == "Login successful"
         assert response.status_code == 200
