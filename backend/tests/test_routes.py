68fedc9d74c31ebff6616292d46328482ba44048
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-05-09 23:13:32 -0400

    refactor: extrapolate user creation method to be reused by other tests

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -5,66 +5,68 @@ import base64
 from flask_sqlalchemy import SQLAlchemy
 
 
+@pytest.fixture(scope="module")
+def client(app: Flask) -> FlaskClient:
+    """Create a test client."""
+    return app.test_client()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clear_database(db: SQLAlchemy):
+    """Set up and tear down the database for each test."""
+    db.drop_all()
+    db.create_all()
+
+
+def create_user(client: FlaskClient, username: str = "testuser", password: str = "Testuser123!"):
+    """Create a user."""
+    return client.post("/api/auth/register", json={"username": username, "password": password})
+
+
 class TestAuthRoutes:
     """Test suite for the auth routes. THESE TESTS ARE IN ORDER."""
 
-    @pytest.fixture
-    def client(self, app: Flask) -> FlaskClient:
-        """Create a test client."""
-        return app.test_client()
-
-    @pytest.fixture(scope="function", autouse=True)
-    def clear_database(self, db: SQLAlchemy):
-        """Set up and tear down the database for each test."""
-        db.drop_all()
-        db.create_all()
-
-    @classmethod
-    def create_user(self, client: FlaskClient, username: str = "testuser", password: str = "Testuser123!"):
-        """Create a user."""
-        return client.post("/api/auth/register", json={"username": username, "password": password})
-
     def test_register_user(self, client: FlaskClient):
         """Test the register user route."""
-        response = self.create_user(client)
+        response = create_user(client)
         assert response.status_code == 200
         assert response.json["message"] == "User registered successfully"
 
     def test_register_user_existing(self, client: FlaskClient):
         """Test the register user route with an existing user."""
-        self.create_user(client)
-        response = self.create_user(client)
+        create_user(client)
+        response = create_user(client)
 
         assert response.status_code == 401
         assert response.json["error"] == "User already exists"
 
     def test_register_user_missing_username(self, client: FlaskClient):
         """Test the register user route with a missing username."""
-        response = self.create_user(client, username=None)
+        response = create_user(client, username=None)
         assert response.status_code == 400
         assert response.json["error"] == "Missing username or password"
 
     def test_register_user_missing_password(self, client: FlaskClient):
         """Test the register user route with a missing password."""
-        response = self.create_user(client, password=None)
+        response = create_user(client, password=None)
         assert response.status_code == 400
         assert response.json["error"] == "Missing username or password"
 
     def test_login_user_empty_credentials(self, client: FlaskClient):
         """Test the login user route with empty credentials."""
-        response = self.create_user(client, username="", password="")
+        response = create_user(client, username="", password="")
         assert response.status_code == 400
         assert response.json["error"] == "Missing username or password"
 
     def test_register_user_invalid_password(self, client: FlaskClient):
         """Test the register user route with an invalid password."""
-        response = self.create_user(client, password="Invalidpassword")
+        response = create_user(client, password="Invalidpassword")
         assert response.status_code == 400
         assert response.json["error"] is not None
 
     def test_login_user(self, client: FlaskClient):
         """Test the login user route."""
-        self.create_user(client)
+        create_user(client)
         encoded_credentials = base64.b64encode(b"testuser:Testuser123!").decode("utf-8")
         response = client.post("/api/auth/login", headers={"Authorization": f"Basic {encoded_credentials}"})
 
@@ -76,7 +78,7 @@ class TestAuthRoutes:
 
     def test_login_user_invalid_credentials(self, client: FlaskClient):
         """Test the login user route with invalid credentials."""
-        self.create_user(client)
+        create_user(client)
         encoded_credentials = base64.b64encode(b"testuser:Invalidpassword").decode("utf-8")
         response = client.post("/api/auth/login", headers={"Authorization": f"Basic {encoded_credentials}"})
 
