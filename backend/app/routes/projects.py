7b2b2cb8ba9313e72d883a00dcae1b6a1d591ce2
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-05-14 14:02:57 -0400

    feat: implement get projects by UUID

--- backend/app/routes/projects.py
+++ backend/app/routes/projects.py
@@ -1,16 +1,28 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, Response
 from app.models import Projects
 
 projects_bp = Blueprint("projects", __name__)
 
 
 @projects_bp.route("/", methods=["GET"])
-def get_projects():
+def get_projects() -> Response:
     """Get all projects.
 
     Returns:
         A JSON response containing all projects.
     """
-    return jsonify(
-        {"message": "Fetched all projects", "projects": [project.to_dict() for project in Projects.query.all()]}
-    ), 200
+    json_data: dict = request.get_json(silent=True) or {}
+    uuid: str | None = json_data.get("uuid")
+
+    projects: list[Projects] = []
+    message: str = ""
+
+    # If nothing is provided, assume all projects are requested
+    if not uuid:
+        projects.extend(Projects.query.all())
+        message = "Fetched all projects"
+    else:
+        projects.append(Projects.query.filter(Projects.uuid == uuid).one_or_none())
+        message = "Fetched project by UUID"
+
+    return jsonify({"message": message, "projects": [project.to_dict() for project in projects]}), 200
