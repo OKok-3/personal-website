f94864fb78b494ff013adbf48458820866fab14a
OKok <53050459+OKok-3@users.noreply.github.com>
2025-08-25 17:32:13 -0400

    feat: set global font

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./globals.css";
+import { Lora } from "next/font/google";
 import Nav from "@/components/nav";
 
 export const metadata = {
@@ -7,11 +8,16 @@ export const metadata = {
   title: "Daniel Guan",
 };
 
+const instrumentSerif = Lora({
+  subsets: ["latin"],
+  weight: "variable",
+});
+
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props;
 
   return (
-    <html lang="en">
+    <html lang="en" className={instrumentSerif.className}>
       <body className="relative flex min-h-screen min-w-screen flex-col bg-neutral-50 dark:bg-gray-900">
         <Nav />
         <main className="flex w-full flex-1 flex-col">{children}</main>
