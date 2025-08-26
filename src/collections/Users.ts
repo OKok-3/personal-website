d1472ed06cd7128e0c0cc24b45b92e3e1ca1c24e
OKok <53050459+OKok-3@users.noreply.github.com>
2025-08-26 18:07:54 -0400

    misc: change Users collection to use name as title

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -3,11 +3,10 @@ import type { CollectionConfig } from "payload";
 export const Users: CollectionConfig = {
   slug: "users",
   admin: {
-    useAsTitle: "email",
+    useAsTitle: "name",
   },
   auth: true,
   fields: [
-    // Email added by default
     {
       name: "name",
       type: "text",
