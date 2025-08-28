d847cdb839614ccb80cc2817c2573238ffae9584
OKok <53050459+OKok-3@users.noreply.github.com>
2025-08-28 15:44:58 -0400

    feat: integrate landing pag with payload cms

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,73 +1,49 @@
 import Image from "next/image";
 import "./globals.css";
+import { getPayload, Payload } from "payload";
+import config from "@/payload.config";
+import type { Icon, LandingPage } from "@/payload-types";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const payload: Payload = await getPayload({ config });
+  const landingPageGlobal: LandingPage = await payload.findGlobal({
+    slug: "landing-page",
+  });
+
+  const content = landingPageGlobal.content?.[0];
+  if (!content) {
+    return <div>Ooops, there seems to be a problem.</div>;
+  }
+
+  const { h1, h2, description, location, socials } = content;
+
   return (
     <div className="align-center flex h-full max-w-full flex-col px-8 pt-[18vh] md:px-20 lg:max-w-280 lg:pt-[25dvh] lg:pl-60">
-      <h1 className="text-2xl font-semibold lg:text-4xl">
-        Hi, my name is Daniel
-      </h1>
-      <h2 className="mt-2 text-xl lg:mt-4 lg:text-2xl">
-        Engineering solutions, learning along the way in tech and finance.
-      </h2>
-      <p className="mt-4 text-sm text-neutral-400 lg:text-lg">
-        I'm Daniel, and I enjoy working with code, data, and infrastructure to
-        build systems that solve real problems. My curiosity drives me to
-        explore quantitative finance alongside technology, creating tools that
-        are both scalable and insightful.
-      </p>
+      <h1 className="text-2xl font-semibold lg:text-4xl">{h1}</h1>
+      <h2 className="mt-2 text-xl lg:mt-4 lg:text-2xl">{h2}</h2>
+      <p className="mt-4 text-sm text-neutral-400 lg:text-lg">{description}</p>
       <div className="mt-10 flex items-center gap-5">
-        <div>
-          <a
-            href="https://www.github.com/OKok-3"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group inline-flex items-center"
-            aria-label="LinkedIn profile"
-          >
-            <Image
-              src="/logos/github.svg"
-              alt="GitHub"
-              width={24}
-              height={24}
-              className="opacity-70 transition-all duration-300 ease-in-out group-hover:opacity-50"
-            />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://www.linkedin.com/in/tong-g"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group inline-flex items-center"
-            aria-label="GitHub profile"
-          >
-            <Image
-              src="/logos/linkedin.svg"
-              alt="LinkedIn"
-              width={24}
-              height={24}
-              className="opacity-70 transition-all duration-300 ease-in-out group-hover:opacity-50"
-            />
-          </a>
-        </div>
-        <div>
-          <a
-            href="mailto:tguan29@uwo.ca"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group inline-flex items-center"
-            aria-label="Email address"
-          >
-            <Image
-              src="/icons/email.svg"
-              alt="Email"
-              width={28}
-              height={28}
-              className="opacity-70 transition-all duration-300 ease-in-out group-hover:opacity-50"
-            />
-          </a>
-        </div>
+        {socials?.map((social) => (
+          <div key={social.platform} className="relative aspect-square h-6">
+            <Link
+              href={social.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group inline-flex items-center"
+              aria-label={`${social.platform} profile`}
+            >
+              <Image
+                src={(social.icon as Icon).url || ""}
+                alt={`${social.platform} icon`}
+                fill
+                priority={true}
+                sizes="1px"
+                className="opacity-70 transition-all duration-300 ease-in-out group-hover:opacity-50"
+              />
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
