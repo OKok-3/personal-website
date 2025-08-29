71e46963b885310d79fb087c6eed408f65bf190d
OKok <53050459+OKok-3@users.noreply.github.com>
2025-08-28 23:33:42 -0400

    style: add animation to landing page content

--- src/components/LandingPage/landing_page_client.tsx
+++ src/components/LandingPage/landing_page_client.tsx
@@ -1,20 +1,65 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import type { LandingPage, Icon } from "@/payload-types";
+import { motion, Variants } from "motion/react";
 
 export default function LandingPageClient(props: {
   content: LandingPage["content"][0];
 }) {
   const { h1, h2, description, location, socials } = props.content;
 
+  const divVariants: Variants = {
+    animate: { transition: { staggerChildren: 0.3 } },
+  };
+
+  const childVariants: Variants = {
+    initial: { opacity: 0, y: 20 },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1, ease: "easeInOut" },
+    },
+    exit: { opacity: 0, y: 20, transition: { duration: 1, ease: "easeInOut" } },
+  };
+
   return (
-    <div className="align-center flex h-full max-w-full flex-col px-8 pt-[18vh] md:px-20 lg:max-w-280 lg:pt-[25dvh] lg:pl-60">
-      <h1 className="text-2xl font-semibold lg:text-4xl">{h1}</h1>
-      <h2 className="mt-2 text-xl lg:mt-4 lg:text-2xl">{h2}</h2>
-      <p className="mt-4 text-sm text-neutral-400 lg:text-lg">{description}</p>
-      <div className="mt-10 flex items-center gap-5">
+    <motion.div
+      className="align-center flex h-full max-w-full flex-col px-8 pt-[18vh] md:px-20 lg:max-w-280 lg:pt-[25dvh] lg:pl-60"
+      variants={divVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+    >
+      <motion.h1
+        className="text-2xl font-semibold lg:text-4xl"
+        variants={childVariants}
+      >
+        {h1}
+      </motion.h1>
+      <motion.h2
+        className="mt-2 text-xl lg:mt-4 lg:text-2xl"
+        variants={childVariants}
+      >
+        {h2}
+      </motion.h2>
+      <motion.p
+        className="mt-4 text-sm text-neutral-400 lg:text-lg"
+        variants={childVariants}
+      >
+        {description}
+      </motion.p>
+      <motion.div
+        className="mt-10 flex items-center gap-5"
+        variants={divVariants}
+      >
         {socials?.map((social) => (
-          <div key={social.platform} className="relative aspect-square h-6">
+          <motion.div
+            key={social.platform}
+            className="relative aspect-square h-6"
+            variants={childVariants}
+          >
             <Link
               href={social.url}
               target="_blank"
@@ -31,9 +76,9 @@ export default function LandingPageClient(props: {
                 className="opacity-70 transition-all duration-300 ease-in-out group-hover:opacity-50"
               />
             </Link>
-          </div>
+          </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
