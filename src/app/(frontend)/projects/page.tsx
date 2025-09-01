a9deda98a5ab9cb62aef4b29f3dc5b7bc54bee15
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-09-01 18:18:51 -0400

    feat: implement projects page server component

--- src/app/(frontend)/projects/page.tsx
+++ src/app/(frontend)/projects/page.tsx
@@ -1,3 +1,17 @@
-export default function Projects() {
+import { getPayload, PaginatedDocs, Payload } from "payload";
+import config from "@/payload.config";
+import { Project } from "@/payload-types";
+
+export default async function Projects() {
+  const payload: Payload = await getPayload({ config });
+  const projects: PaginatedDocs<Project> = await payload.find({
+    collection: "projects",
+    where: {
+      published: {
+        equals: true,
+      },
+    },
+  });
+
   return <div>Projects</div>;
 }
