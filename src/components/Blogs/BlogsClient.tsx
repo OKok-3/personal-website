e4c157dc608356b3e9e1fedc888000c5f2f2da38
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-09-02 17:29:57 -0400

    feat: use the card component for blogs

--- src/components/Blogs/BlogsClient.tsx
+++ src/components/Blogs/BlogsClient.tsx
@@ -1,7 +1,27 @@
-import type { Blog } from "@/payload-types";
+import type { Blog, Media, Tag } from "@/payload-types";
+import Card from "@/components/Card";
 
 export default function BlogsClient(props: { blogs: Blog[] }) {
   const { blogs } = props;
 
-  return <div>{blogs.map((blog) => blog.title)}</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-4xl font-medium lg:text-5xl">Blogs</h1>
+      <p>Here are some of the blogs I've written.</p>
+      <div>
+        {blogs.map((blog) => (
+          <Card
+            key={blog.id}
+            id={blog.id}
+            title={blog.title}
+            publishedAtRaw={blog.publishedAt as string}
+            category={blog.category as Tag}
+            description={blog.tagLine}
+            coverImage={blog.coverImage as Media}
+            blog={blog}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
