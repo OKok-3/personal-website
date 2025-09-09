b0ea76ddd74edc9bcb3693e23b93ad2f4aa37f7c
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-09-08 21:14:00 -0400

    fix: enforce proper typing for the code block Block item

--- src/blocks/CodeBlock.ts
+++ src/blocks/CodeBlock.ts
@@ -1,4 +1,4 @@
-import { Block } from "payload";
+import type { Block } from "payload";
 
 const languages = {
   Python: "python",
@@ -15,6 +15,7 @@ const languages = {
 
 export const CodeBlock: Block = {
   slug: "codeBlock",
+  interfaceName: "CodeBlock",
   fields: [
     {
       name: "filename",
