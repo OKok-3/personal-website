b4a5d331ba8bff7da49f2d44e31db7d4417a7cb8
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-09-09 15:27:50 -0400

    feat: enhance callout block with dynamic styling and icons

--- src/components/RichText/Nodes/Callout.tsx
+++ src/components/RichText/Nodes/Callout.tsx
@@ -2,11 +2,61 @@
 
 import { SerializedBlockNode } from "@payloadcms/richtext-lexical";
 import { Callout as CalloutType } from "@/payload-types";
+import Image from "next/image";
+
+const colorMap = {
+  info: {
+    icon: "/icons/info.svg",
+    bgColor: "bg-blue-200/70",
+    solidBorderColor: "border-blue-400",
+    dashedBorderColor: "border-blue-400",
+  },
+  warning: {
+    icon: "/icons/warning.svg",
+    bgColor: "bg-orange-200/70",
+    solidBorderColor: " border-orange-400",
+    dashedBorderColor: "border-orange-400",
+  },
+  error: {
+    icon: "/icons/error.svg",
+    bgColor: "bg-red-200/70",
+    solidBorderColor: "border-red-400",
+    dashedBorderColor: "border-red-400",
+  },
+  success: {
+    icon: "/icons/success.svg",
+    bgColor: "bg-emerald-200/70",
+    solidBorderColor: "border-green-400",
+    dashedBorderColor: "border-green-400",
+  },
+};
 
 export const CalloutNode: React.FC<{
   node: SerializedBlockNode<CalloutType>;
 }> = ({ node }) => {
-  const { type, title, content } = node.fields;
+  const {
+    type,
+    title = type.charAt(0).toUpperCase() + type.slice(1),
+    content,
+  } = node.fields;
 
-  return <div>{content}</div>;
+  const { icon, bgColor, solidBorderColor, dashedBorderColor } = colorMap[type];
+
+  return (
+    <div
+      className={`${solidBorderColor} my-4 overflow-hidden rounded-sm border last:mb-0`}
+    >
+      <div className={`${bgColor} flex items-center justify-start gap-2 p-2`}>
+        <div className="relative aspect-square h-5">
+          <Image src={icon} alt={type} fill />
+        </div>
+        <h3 className={`text-md font-medium`}>{title}</h3>
+      </div>
+      <p
+        className={`${dashedBorderColor} border-t-1 border-dashed p-2 text-sm`}
+      >
+        {content}
+      </p>
+    </div>
+  );
 };
