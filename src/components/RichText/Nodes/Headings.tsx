66646f3b6b6772101692b20092bb84204024c2a0
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-09-30 13:52:37 -0400

    style: adjust heading spacing

--- src/components/RichText/Nodes/Headings.tsx
+++ src/components/RichText/Nodes/Headings.tsx
@@ -10,12 +10,12 @@ import type {
 
 // Map heading levels to Tailwind classes
 const headingStyles = {
-  h1: "text-2xl font-bold lg:text-3xl",
-  h2: "text-xl font-bold lg:text-2xl",
-  h3: "text-lg font-semibold lg:text-xl",
-  h4: "text-md font-semibold lg:text-lg",
-  h5: "text-md font-medium lg:text-lg",
-  h6: "text-md font-medium lg:text-lg",
+  h1: "text-2xl font-bold lg:text-3xl mt-8",
+  h2: "text-xl font-bold lg:text-2xl mt-6",
+  h3: "text-lg font-semibold lg:text-xl mt-4",
+  h4: "text-md font-semibold lg:text-lg mt-2",
+  h5: "text-md font-medium lg:text-lg mt-2",
+  h6: "text-md font-medium lg:text-lg mt-2",
 } as const;
 
 export const Heading: React.FC<{ node: SerializedHeadingNode }> = ({
