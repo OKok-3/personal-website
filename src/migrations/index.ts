2276dc915a77f4160c886a1095af22bebc1a57b3
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-11-02 21:51:02 -0500

    fix: add missing db migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,21 +1,27 @@
-import * as migration_20250926_194546_initial from "./20250926_194546_initial";
-import * as migration_20250927_191734_remove_sub_menu_item from "./20250927_191734_remove_sub_menu_item";
-import * as migration_20250929_140037_callout_block_textArea_to_richText from "./20250929_140037_callout_block_textArea_to_richText";
+import * as migration_20250926_194546_initial from './20250926_194546_initial';
+import * as migration_20250927_191734_remove_sub_menu_item from './20250927_191734_remove_sub_menu_item';
+import * as migration_20250929_140037_callout_block_textArea_to_richText from './20250929_140037_callout_block_textArea_to_richText';
+import * as migration_20251103_025002 from './20251103_025002';
 
 export const migrations = [
   {
     up: migration_20250926_194546_initial.up,
     down: migration_20250926_194546_initial.down,
-    name: "20250926_194546_initial",
+    name: '20250926_194546_initial',
   },
   {
     up: migration_20250927_191734_remove_sub_menu_item.up,
     down: migration_20250927_191734_remove_sub_menu_item.down,
-    name: "20250927_191734_remove_sub_menu_item",
+    name: '20250927_191734_remove_sub_menu_item',
   },
   {
     up: migration_20250929_140037_callout_block_textArea_to_richText.up,
     down: migration_20250929_140037_callout_block_textArea_to_richText.down,
-    name: "20250929_140037_callout_block_textArea_to_richText",
+    name: '20250929_140037_callout_block_textArea_to_richText',
+  },
+  {
+    up: migration_20251103_025002.up,
+    down: migration_20251103_025002.down,
+    name: '20251103_025002'
   },
 ];
